601fec2dd92ca22e179393320b7b8f3fe5206d62
unknown <moradmah0@gmail.com>
2020-10-10 04:18:56 +0100

    first commit

---
+++ Notification/Controllers/SimulationController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Notification.Controllers
+{
+    public class SimulationController : Controller
+    {
+        // GET: Simulation
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult GetNotificationContacts()
+        {
+            var notificationRegisterTime = Session["LastUpdated"] != null ? Convert.ToDateTime(Session["LastUpdated"]) : DateTime.Now;
+            NotificationComponent NC = new NotificationComponent();
+            var list = NC.GetContacts(notificationRegisterTime);
+            //update session here for get only new added contacts (notification)
+            Session["LastUpdate"] = DateTime.Now;
+            return new JsonResult { Data = list, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+        }
+    }
+}
